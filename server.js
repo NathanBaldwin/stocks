1eb5adc556a19ac2a5cd5486dd17de11f8ba8247
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-02-15 19:21:20 -0600

    added views directory. First get request succeeds

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 //load express
-
 const express = require('express');
 const app = express(); //what is happening here?
 
+//dif between res.render and res.send?
+
+//if port is not provided, port is 3000
 const PORT = process.env.PORT || 3000;
 
+//link to routes module:
+const routes = require('./routes/')
+
+//configure express to use jade:
+app.set('view engine', 'jade');
+
+
+app.get('/requestQuote', (req, res) => {
+  res.render('requestQuote');
+})
 
 app.listen(PORT, () => {
     console.log(`Node.js server started. Listening on port ${PORT}`);
